1952ca98f1711b1a672e39656be3a78814af8b3e
Chitresh8 <123091567+Chitresh8@users.noreply.github.com>
2023-08-16 20:00:18 +0530

    Initial Commit

---
+++ Array method/script.js
@@ -0,0 +1,26 @@
+//Switch case method
+
+const fruits = [
+  "Apple",
+  "Banana",
+  "CustardApple",
+  "Dates",
+  "Elderberries",
+  "Figs",
+  "Grapes",
+  "HoneyDewLemons",
+  "JackFruit",
+  "Kiwis",
+  "Lemons",
+  "Mangoes",
+  "Nectarines",
+  "Oranges",
+  "Pears",
+  "Quince",
+  "Raisins",
+  "Satsumas",
+  "Watermelons",
+];
+
+let fruitsList = fruits.length;
+console.log("The Total List of Fruits is : " + fruitsList);
